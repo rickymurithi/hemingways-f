e889e31c8a6168174dfacf77636adbce5905aa1f
Abdulaziz Abdullahi <abdulaziz.abdullahi@student.moringaschool.com>
2022-11-08 22:49:42 +0300

    create the resort descriptions

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,3 +5,21 @@ import "../css/style.css"
 // import { Route } from 'react-router-dom';
 import { useRouteMatch } from 'react-router-dom';
 // import Subscribe from './Subscribe';
+
+        <div className='container1'>
+            <div className='container2'>
+                <img src={img} alt="resort" />
+            </div>
+            <div className="container">
+                <div className='container3'>
+                    <p>A warm welcome to Hemingways Nairobi. A luxury five Star boutique hotel in Nairobi. We pride ourselves on providing exceptional guest experiences and service, ensuring the utmost comfort and luxury.</p>
+                </div>
+                <div className="cont">
+                    <div className='container4'>
+                        <p>Your dedicated butler will know that you prefer Earl Grey to Assam with your scones, and will remember which trousers you like pressed flat and which ones need a central crease.<br />So while you are indulging in the spa, relaxing beside the pool or meandering through the gardens, back in your suite, your butler will have dimmed the lights, adjusted your personalized pillows, set out your nightcap and arranged the morning’s outing down to the finest detail.</p>
+                    </div>
+                    <div className='container5'>
+                        <p>This 45-suite boutique hotel elevates the standard of luxury for travellers in the region. Beautiful in its symmetry and flooded with natural light, this plantation-style property is airy and spacious. Each of our 80m² suites have a private balcony overlooking the iconic silhouette of the Ngong Hills, a magical view which can also be enjoyed over sundowners at the Bar.<br /> <br />Our service is personal and sincere, offering a luxury experience from the moment you touchdown in Kenya.</p>
+                    </div>
+                </div>
+            </div>
