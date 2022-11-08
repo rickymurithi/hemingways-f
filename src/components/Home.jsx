3714322775cd7020d1b95b23df3f272b0a4670f4
Abdulaziz Abdullahi <abdulaziz.abdullahi@student.moringaschool.com>
2022-11-08 22:48:05 +0300

    add imports in home components

---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+import React,{useState,useEffect} from 'react';
+import img from '../images/hemingways.png';
+import deluxe from '../images/deluxe_suite.jpg';
+import "../css/style.css"
+// import { Route } from 'react-router-dom';
+import { useRouteMatch } from 'react-router-dom';
+// import Subscribe from './Subscribe';
