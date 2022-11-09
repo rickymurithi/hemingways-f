8c16f63596a081f57be0e9132eb8b819c3a050e9
Pronepoet <ochiengphillip84@gmail.com>
2022-11-09 15:04:07 +0300

    Added code for the offers component

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 import "../css/style.css"
-function Offers(){
 
-}
+function Offers(){
+    const [offers, setOffer]=useState([]);
+    useEffect(()=>{
+        fetch('https://hemingways-backend.herokuapp.com/offers')
+        .then(res=>res.json())
+        .then(data=>setOffer(data))
+    },[])
+    
+     const cards=offers.map((offer)=>{
+        return (
+        <div key={offer.type} className="offerCard">
+            <img src={offer.imgUrl} alt={offer.type} />
+            <h1>{offer.type}</h1>
+            <p>Current price:{offer.currentPrice}</p>
+            <p>Previous price:{offer.previousPrice}</p>
+            <p>{offer.off} off</p>
+        </div>)
+     })
+        return (
+    
+           <div className='cont5'>
+            <h1>Current offers</h1>
+            <h2>Get them while on discount!!</h2>
+            {cards}
+            </div>
+        
+        )
+    }
 
 export default Offers;
