2c62b498881aaee15ff42968c3a58ef4c2cc2cbf
Abdulaziz Abdullahi <abdulaziz.abdullahi@student.moringaschool.com>
2022-11-10 19:28:09 +0300

    add routes to different pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,35 @@
 import Home from './Home'
 import {Route, Switch} from 'react-router-dom'
-
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Feedback from './Feedback';
+import Book from './Book';
+import Subscribe from './Subscribe';
+import Offers from './Offers';
 
 
 function App() {
   return (
       <>
+      <Navbar/>
       <Switch>
+        <Route exact path="/feedback">
+          <Feedback/>
+        </Route>
+        <Route exact path="/book">
+          <Book/>
+        </Route>
+        <Route exact path="/contact">
+          <Subscribe/>
+        </Route>
+        <Route exact path="/offers">
+          <Offers/>
+        </Route>
         <Route exact path="/">
           <Home/>
         </Route>
       </Switch>
+      <Footer/>
       </>
   );
 }
