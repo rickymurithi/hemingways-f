8cca2fdd125bdc9d257552e16125c62bb70f3313
Abdulaziz Abdullahi <abdulaziz.abdullahi@student.moringaschool.com>
2022-11-11 11:02:24 +0300

    made changes to fetch end point to local server

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -7,12 +7,12 @@ function Book(){
         Name:'',
         // lname:'',
         email:'',
-        number:'',
-        type:'executive_suites'
+        phone_number:'',
+        suite:'executive_suites'
     })
     function handleSubmit(e){
         e.preventDefault()
-        fetch('https://hemingways-backend.herokuapp.com/booked',{
+        fetch('http://localhost:9292/bookings',{
             method: "POST",
             headers: {
               "Content-Type": "application/json",
@@ -44,16 +44,16 @@ function Book(){
                 <h2>Fill the form below to kick start your stay with us</h2>
                 <div className="cont4">
                     <form onSubmit={handleSubmit} >
-                        <label htmlFor="fname">Name : </label>
+                        <label htmlFor="Name">Name : </label>
                         <input required type="text" name="Name" id="Name" onChange={handleChange} value={formData.Name} placeholder="Enter your full name"/><br /><br />
                         {/* <label htmlFor="lname">Last name : </label>
                         <input required type="text" name="lname" id="lname" onChange={handleChange} value={formData.lname} placeholder="Enter your last name"/><br /><br /> */}
                         <label htmlFor="email">Email : </label>
                         <input className="email4"required type="email" name="email" id="email" onChange={handleChange} value={formData.email} placeholder="Enter your email" /><br /><br />
-                        <label htmlFor="number">Phone number : </label>
-                        <input required type="text" name="no." id="number" onChange={handleChange} value={formData.number} maxLength={10}placeholder="Enter your phone number"/> <br /> <br />
-                        <label htmlFor="type">Choose the suite you would like : </label> 
-                        <select id='type' onChange={handleChange} value={formData.type} >
+                        <label htmlFor="phone_number">Phone number : </label>
+                        <input required type="text" name="no." id="phone_number" onChange={handleChange} value={formData.phone_number} maxLength={10}placeholder="Enter your phone number"/> <br /> <br />
+                        <label htmlFor="suite">Choose the suite you would like : </label> 
+                        <select id='suite' onChange={handleChange} value={formData.suite} >
                             <option value="executive_suites">The Executive Suites</option>
                             <option value="hemingway_suites">The Hemingway Suites</option>
                             <option value="deluxe_suites">Deluxe Suites</option>
